5d81f31ff7e9085f63b1f005c3c1c2009f8f62da
Zhanbolat Dekstop <kussainov.zhanbolat@gmail.com>
2019-01-31 15:06:42 +0600

    Initial commit

---
+++ Week 1/Task 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Student {
+        string name;
+        string id;
+        int year;
+
+        public Student(string name, string id) {
+            this.name = name;
+            this.id = id;
+            this.year = 0;
+        }
+        public void getName() {
+            Console.WriteLine ("Name: " + name);
+        }
+        public void getId() {
+            Console.WriteLine ("Id: " + id);
+        }
+        public void incYear () {
+            year++;
+        }
+        public void  getYear  () {
+            Console.WriteLine ("Year: " + year.ToString());
+        }
+        public void PrintInfo() {
+            getName();
+            getId();
+            getYear();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student ("Jigurda", "18BD7777777");
+            s1.incYear();
+            s1.PrintInfo();
+            s1.incYear();
+            s1.PrintInfo();
+        }
+    }
+}
---
+++ Week 1/Task 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse (Console.ReadLine()); // reading n
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    Console.Write ("[*]");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
