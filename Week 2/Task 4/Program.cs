30b7d2504dc95e996d0039d8aae8150a92beab4d
Zhanbolat Dekstop <kussainov.zhanbolat@gmail.com>
2019-02-14 00:32:16 +0600

    C#

--- Week 2/Task 4/Program.cs
+++ Week 2/Task 4/Program.cs
@@ -8,38 +8,21 @@ namespace Task_4
         static void Main(string[] args)
         {
 
-            DirectoryInfo path1 = new DirectoryInfo (@"C:\Olymp\C#\PP2\Programming-Principles-2-KBTU\Week 2\Task 4\source folder");
-            DirectoryInfo path2 = new DirectoryInfo (@"C:\Olymp\C#\PP2\Programming-Principles-2-KBTU\Week 2\Task 4\destination");
-
-            // Console.ForegroundColor = ConsoleColor.Yellow;
-            // Console.WriteLine ("BEFORE");
-            // Console.ForegroundColor = ConsoleColor.White;
-
-
-            // show (path1);
-            // show (path2);
+            DirectoryInfo path1 = new DirectoryInfo (@"C:\Olymp\C#\PP2\Programming-Principles-2-KBTU\Week 2\Task 4\source folder");  // create directory variable for source folder
+            DirectoryInfo path2 = new DirectoryInfo (@"C:\Olymp\C#\PP2\Programming-Principles-2-KBTU\Week 2\Task 4\destination"); // create directory variable for destinational folder
+            string afile = Console.ReadLine(); // read the name of the file to be moved
             
-            // Console.WriteLine ("Enter the file name");
-            string afile = Console.ReadLine();
-            
-            string sfile = Path.Combine(path1.FullName, afile);
-            string ffile =  Path.Combine(path2.FullName, afile);
-            if (!File.Exists (sfile)) {
-
-                Console.WriteLine ("File was not found, new instnace was created");
-                File.WriteAllText (sfile, afile + " was created");
+            string sfile = Path.Combine(path1.FullName, afile); // a path for source file
+            string ffile =  Path.Combine(path2.FullName, afile); // path for destination file
+            if (!File.Exists (sfile)) { // if the source file exists, then create a new file with the text "$file_name$ + was created"
+                Console.WriteLine ("File was not found, new instnace was created");  // decribiing the process
+                File.WriteAllText (sfile, afile + " was created"); // create a file and write a text in it.
             }
-            Console.WriteLine ("Moving a file \n From {0}\n To {1}", sfile, ffile);
-            File.Move (sfile, ffile);
-            Console.WriteLine ("Moving completed.");
-            // Console.ForegroundColor = ConsoleColor.Green;
-            // Console.WriteLine ("AFTER");
-            //Console.ForegroundColor = ConsoleColor.White;
-
-            // show (path1);
-            // show (path2);
+            Console.WriteLine ("Moving a file \n From {0}\n To {1}", sfile, ffile); // describing following process
+            File.Move (sfile, ffile); // moving a file
+            Console.WriteLine ("Move completed."); // the process is done.
         }
-        static void show (DirectoryInfo x) {
+        static void show (DirectoryInfo x) { // used this function to print a directory content for testing. Not used in main function
             Console.WriteLine (x.FullName);
             var files = x.GetFileSystemInfos();
             
